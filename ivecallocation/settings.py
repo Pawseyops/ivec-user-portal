f63c122608983f40a38c1cb98d0aa764cbd3fd43
cwellington <cwellington@localhost>
2011-05-30 06:52:01 +0000

    registration form changes

--- ivecallocation/settings.py
+++ ivecallocation/settings.py
@@ -173,4 +173,6 @@ LOGS = []                                               # list the logs you want
 # registration app settings
 ACCOUNT_ACTIVATION_DAYS = 14
 
+RECAPTCHA_PUB_KEY = "6LcB0sQSAAAAAJv_KVDzDjRbAMpJfc9b2t6rlOdV"
+RECAPTCHA_PRIV_KEY = "6LcB0sQSAAAAAPy2brzHv_A6i3Atft6rmrfC32-g"
 
