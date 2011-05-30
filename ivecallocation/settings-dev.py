ed73e6486d5ee6690f41e10917265963c9884d5a
andrew <andrew@localhost>
2011-05-30 08:14:10 +0000

    base templates

--- ivecallocation/settings-dev.py
+++ ivecallocation/settings-dev.py
@@ -1,177 +1,63 @@
 # -*- coding: utf-8 -*-
-# Django settings for ivecallocation project.
+# Django settings for project.
 import os
-PROJECT_DIRECTORY = os.environ['PROJECT_DIRECTORY']
-SCRIPT_NAME = os.environ['SCRIPT_NAME']
+from django.utils.webhelpers import url
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+# PROJECT_DIRECTORY isnt set when not under wsgi
+if not os.environ.has_key('PROJECT_DIRECTORY'):
+    os.environ['PROJECT_DIRECTORY']=os.path.dirname(__file__).split("/appsettings/")[0]
 
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-)
-
-MANAGERS = ADMINS
-
-DEBUG = True if os.path.exists(os.path.join(PROJECT_DIRECTORY,".debug")) else DEBUG
-TEMPLATE_DEBUG = DEBUG
-
-SSL_ENABLED = False
-DEV_SERVER = True
-    
-# debug site table
-SITE_ID = 1
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'dev_ivecallocation',                      # Or path to database file if using sqlite3.
-        'USER': 'ivecallocationapp',                      # Not used with sqlite3.
-        'PASSWORD': 'ivecallocationapp',                  # Not used with sqlite3.
-        'HOST': 'eowyn.localdomain',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/Chicago'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
-USE_L10N = True
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'km=+$gnpqosnhis4rc36yjj7%6fgff6zv6=aa)xnzvns)f)!*s'
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-
-)
-
-MIDDLEWARE_CLASSES = (
-    #'project.EmailExceptionMiddleware.EmailExceptionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    #'django.middleware.doc.XViewMiddleware',
-    #'project.middleware.SSLRedirect'
-)
+from appsettings.default_dev import *
+from appsettings.ivecallocation.dev import *
 
 ROOT_URLCONF = 'ivecallocation.urls'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    "templates"
-)
+INSTALLED_APPS.extend( [
+    'ivecallocation.allocation',
+    'ivecallocation.registration'
+] )
 
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'registration',
-    'django.contrib.messages',
-    # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
-    'registration',
-    'allocation'
-)
+AUTHENTICATION_BACKENDS = [
+ 'django.contrib.auth.backends.ModelBackend',
+]
 
-# a directory that will be writable by the webserver, for storing various files...
-WRITABLE_DIRECTORY = os.path.join(PROJECT_DIRECTORY,"scratch")
+SESSION_COOKIE_PATH = url('/')
+SESSION_SAVE_EVERY_REQUEST = True
+CSRF_COOKIE_NAME = "csrftoken_IVECALLOCATION"
 
-# Captcha image directory
-CAPTCHA_IMAGES = os.path.join(WRITABLE_DIRECTORY, "captcha")
+
+APPEND_SLASH = True
+SITE_NAME = 'ivecallocation'
+
+# ssl for entire site
+SSL_FORCE = True
 
 ##
-## Mako settings stuff
+## LOGGING
 ##
+import logging
+LOG_DIRECTORY = os.path.join(PROJECT_DIRECTORY,"logs")
+LOGGING_LEVEL = logging.DEBUG
+LOGGING_FORMATTER = logging.Formatter('[%(name)s:%(levelname)s:%(filename)s:%(lineno)s:%(funcName)s] %(message)s')
+LOGS = ['ivecallocation']
 
-# extra mako temlate directories
-MAKO_TEMPLATE_DIRS = ( os.path.join(PROJECT_DIRECTORY,"templates","mako"), )
 
-# mako compiled templates directory
-MAKO_MODULE_DIR = os.path.join(WRITABLE_DIRECTORY, "templates")
+# registration app settings
+ACCOUNT_ACTIVATION_DAYS = 14
 
-# mako module name
-MAKO_MODULENAME_CALLABLE = ''
-
-##
-## memcache server list
-##
-MEMCACHE_SERVERS = ['memcache1.localdomain:11211','memcache2.localdomain:11211']
-MEMCACHE_KEYSPACE = ""
 
 ##
 ## CAPTCHA settings
 ##
+
+# Captcha image directory
+CAPTCHA_IMAGES = os.path.join(WRITABLE_DIRECTORY, "captcha")
+
 # the filesystem space to write the captchas into
 CAPTCHA_ROOT = os.path.join(MEDIA_ROOT, 'captchas')
 
 # the URL base that points to that directory served out
 CAPTCHA_URL = os.path.join(MEDIA_URL, 'captchas')
-
-
-##
-## Auth settings
-##
-#AUTH_LDAP_SERVER = ['ldaps://fds2.localdomain','ldaps://fds1.localdomain']
-#AUTH_LDAP_USER_BASE = 'ou=People,dc=ccg,dc=murdoch,dc=edu,dc=au'
-#AUTH_LDAP_GROUP_BASE = 'ou=Nutrition,ou=Web Groups,dc=ccg,dc=murdoch,dc=edu,dc=au'
-#AUTH_LDAP_GROUP = 'User'
-#DEFAULT_GROUP = "baseuser"
-
-##
-## Log settings
-##
-LOGS = []                                               # list the logs you want to generate here ( eg. LOGS = ['app1','app2','system'] )
-
-##
-## Starting cookie settings
-##
-#from django.utils.webhelpers import url
-#SESSION_COOKIE_PATH = url('/')
-#SESSION_SAVE_EVERY_REQUEST = True
-
-
-# registration app settings
-ACCOUNT_ACTIVATION_DAYS = 14
 
 
 RECAPTCHA_PUBLIC_KEY = "6LcB0sQSAAAAAJv_KVDzDjRbAMpJfc9b2t6rlOdV"
