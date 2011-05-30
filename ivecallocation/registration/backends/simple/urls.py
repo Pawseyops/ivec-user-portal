61f1b86d37b1ac8edceb88dd266d5e8be9db037d
cwellington <cwellington@localhost>
2011-05-30 06:06:40 +0000

    IVEC backend

--- ivecallocation/registration/backends/simple/urls.py
+++ ivecallocation/registration/backends/simple/urls.py
@@ -6,7 +6,7 @@ If the default behavior of these views is acceptable to you, simply
 use a line like this in your root URLconf to set up the default URLs
 for registration::
 
-    (r'^accounts/', include('registration.backends.simple.urls')),
+    (r'^accounts/', include('registration.backends.ivec.urls')),
 
 This will also automatically set up the views in
 ``django.contrib.auth`` at sensible default locations.
@@ -24,12 +24,27 @@ from django.views.generic.simple import direct_to_template
 from registration.views import activate
 from registration.views import register
 
-
 urlpatterns = patterns('',
+                       url(r'^activate/complete/$',
+                           direct_to_template,
+                           {'template': 'registration/activation_complete.html'},
+                           name='registration_activation_complete'),
+                       # Activation keys get matched by \w+ instead of the more specific
+                       # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
+                       # that way it can return a sensible "invalid key" message instead of a
+                       # confusing 404.
+                       url(r'^activate/(?P<activation_key>\w+)/$',
+                           activate,
+                           {'backend': 'registration.backends.ivec.IVECBackend'},
+                           name='registration_activate'),
                        url(r'^register/$',
                            register,
-                           {'backend': 'registration.backends.simple.SimpleBackend'},
+                           {'backend': 'registration.backends.ivec.IVECBackend'},
                            name='registration_register'),
+                       url(r'^register/complete/$',
+                           direct_to_template,
+                           {'template': 'registration/registration_complete.html'},
+                           name='registration_complete'),
                        url(r'^register/closed/$',
                            direct_to_template,
                            {'template': 'registration/registration_closed.html'},
