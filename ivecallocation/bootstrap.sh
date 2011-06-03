4811b3117905df2e7ae6ba1d2884588413d6b2cc
ahunter <ahunter@localhost>
2011-06-03 06:31:41 +0000

    we dont need to destroy python path anymore

--- ivecallocation/bootstrap.sh
+++ ivecallocation/bootstrap.sh
@@ -36,18 +36,38 @@ then
     # create a virtual python in the current directory
     python $VIRTUALENV/build/lib*/virtualenv.py --no-site-packages $VPYTHON_DIR
 
+    # we use fab for deployments
     ./$VPYTHON_DIR/bin/easy_install fabric
 
     # install all the eggs in this app
     ./$VPYTHON_DIR/bin/easy_install eggs/*
 
+    # now we are going to eggify app settings, so we can run it locally
+    # we need to jump through a few legacy hoops to make this happen
+
+    if [ ! -d tmp ]
+    then
+        mkdir tmp
+    fi
+    cd tmp
+    rm -rf ccgapps-settings
+    svn export svn+ssh://ccg.murdoch.edu.au/store/techsvn/ccg/ccgapps-settings
+    # the directory has the wrong name, so create a sym link with the name we need
+    ln -s ccgapps-settings appsettings
+    # the setup.py is at the wrong level
+    mv appsetting/setup.py .
+    ../$VPYTHON_DIR/bin/python setup.py bdist_egg
+    ../$VPYTHON_DIR/bin/easy_install dist/*.egg
+    rm -rf appsettings ccgapps-settings
+    cd ..
 
     # hack activate to set some environment we need
     # this version nukes the python path
     echo "PROJECT_DIRECTORY=`pwd`;" >>  $VPYTHON_DIR/bin/activate
-    echo "PYTHONPATH=/usr/local/etc/ccgapps:/usr/local/etc/ccgbuild" >>  $VPYTHON_DIR/bin/activate
+    #echo "PYTHONPATH=/usr/local/etc/ccgapps:/usr/local/etc/ccgbuild" >>  $VPYTHON_DIR/bin/activate
 
-    echo "export PROJECT_DIRECTORY PYTHONPATH" >>  $VPYTHON_DIR/bin/activate
+    #echo "export PROJECT_DIRECTORY PYTHONPATH" >>  $VPYTHON_DIR/bin/activate
+    echo "export PROJECT_DIRECTORY " >>  $VPYTHON_DIR/bin/activate
 
 fi
 
