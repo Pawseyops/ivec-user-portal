3b25cd71dc6b54a38e5837023098c72ab863e095
fjanon <fjanon@localhost>
2011-06-06 09:35:43 +0000

    Created the PublicationForm to set the field length and width

--- ivecallocation/allocation/admin_forms.py
+++ ivecallocation/allocation/admin_forms.py
@@ -11,8 +11,16 @@ class ApplicationForm(forms.ModelForm):
         self.fields["computational_methodology"].widget = forms.Textarea(attrs={'rows':16, 'cols':100})
 
         self.fields["data_transfers"].widget = forms.Textarea(attrs={'rows':6, 'cols':100}) #field is 512 chars max in model
-        self.fields["reference"].widget = forms.Textarea(attrs={'rows':3, 'cols':100}) #field is 256 chars max in model
-        self.fields["description"].widget = forms.Textarea(attrs={'rows':3, 'cols':100}) # verbose name: 'significance', field is 256 chars max in model
 
     class Meta:
         model = Application
+
+
+class PublicationForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(PublicationForm, self).__init__(*args, **kwargs)
+        self.fields["reference"].widget = forms.Textarea(attrs={'rows':3, 'cols':100}) # field is 256 chars max in model
+        self.fields["description"].widget = forms.Textarea(attrs={'rows':3, 'cols':100}) # verbose name: 'significance', field is 256 chars max in model
+
+    class Meta:
+        model = Publication
