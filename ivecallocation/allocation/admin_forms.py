b94b7a0199734a94eac03aac251ff45a2e2a900c
fjanon <fjanon@localhost>
2011-06-07 06:51:16 +0000

    field sizes

--- ivecallocation/allocation/admin_forms.py
+++ ivecallocation/allocation/admin_forms.py
@@ -5,8 +5,8 @@ class ApplicationForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(ApplicationForm, self).__init__(*args, **kwargs)
         self.fields["project_title"].widget = forms.TextInput(attrs={'size':'100'})
-        self.fields["project_summary"].widget = forms.Textarea(attrs={'rows':16, 'cols':100})
-        self.fields["research_record"].widget = forms.Textarea(attrs={'rows':16, 'cols':100})
+        self.fields["project_summary"].widget = forms.Textarea(attrs={'rows':16, 'cols':120})
+        self.fields["research_record"].widget = forms.Textarea(attrs={'rows':16, 'cols':120})
         self.fields["research_significance"].widget = forms.Textarea(attrs={'rows':16, 'cols':100})
         self.fields["computational_methodology"].widget = forms.Textarea(attrs={'rows':16, 'cols':100})
 
@@ -19,8 +19,8 @@ class ApplicationForm(forms.ModelForm):
 class PublicationForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(PublicationForm, self).__init__(*args, **kwargs)
-        self.fields["reference"].widget = forms.Textarea(attrs={'rows':3, 'cols':100}) # field is 256 chars max in model
-        self.fields["description"].widget = forms.Textarea(attrs={'rows':3, 'cols':100}) # verbose name: 'significance', field is 256 chars max in model
+        self.fields["reference"].widget = forms.Textarea(attrs={'rows':4, 'cols':80}) # field is 256 chars max in model
+        self.fields["description"].widget = forms.Textarea(attrs={'rows':4, 'cols':80}) # verbose name: 'significance', field is 256 chars max in model
 
     class Meta:
         model = Publication
