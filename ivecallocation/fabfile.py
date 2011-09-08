0d280c7877e495809daf717656d77a51d08c745a
aivins <aivins@localhost>
2011-09-08 15:36:11 +0800

    Modified fabfile to work with hg repository on Google Code

--- ivecallocation/fabfile.py
+++ ivecallocation/fabfile.py
@@ -4,10 +4,8 @@ from ccgfab.base import *
 env.app_root = '/usr/local/python/ccgapps/'
 env.app_name = 'ivecallocation'
 env.app_install_names = ['ivecallocation'] # use app_name or list of names for each install
-env.vc = 'svn'
-env.git_trunk_url = ""
-env.svn_trunk_url = "svn+ssh://store.localdomain/store/techsvn/ccg/ivecallocation/trunk/"
-env.svn_tags_url = "svn+ssh://store.localdomain/store/techsvn/ccg/ivecallocation/tags/"
+env.vc = 'mercurial'
+env.repo_path = 'ivecallocation'
 
 env.writeable_dirs.extend(["scratch"]) # add directories you wish to have created and made writeable
 env.content_excludes.extend([]) # add quoted patterns here for extra rsync excludes
