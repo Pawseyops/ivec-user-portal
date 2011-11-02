3789404c9a7eb992d5bfd22d0ba506704f8702f3
aivins <aivins@ccg.murdoch.edu.au>
2011-11-02 16:23:43 +0800

    Added the admin login box to the welcome index page. Moved the registration feature out of the main page.

--- ivecallocation/urls.py
+++ ivecallocation/urls.py
@@ -10,6 +10,7 @@ from django.views.generic.simple import redirect_to
 import admin
 
 from django.utils.webhelpers import url as wh_url
+from django.contrib.auth.forms import PasswordResetForm
 
 urlpatterns = patterns('',
     # Example:
@@ -19,18 +20,23 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
+    (r'^', include(admin.site.urls)),
+    url(r'^(?P<usertype>director)/', include(admin.site.urls), name='directors'),
+    
         
     # Uncomment this line to enable the mango status system
     #(r'^project_status', status_view),
     
     # registration
+    (r'^accounts/login', redirect_to, {'url': wh_url('/')}),
     (r'^accounts/', include('registration.backends.ivec.urls')),
     
-    (r'^$', redirect_to, {'url': wh_url('/accounts/register/')}),
-    (r'^login[/]$', redirect_to, {'url': wh_url('/admin/')}),
-)
+    #(r'^$', redirect_to, {'url': wh_url('/accounts/register/')}),
+    (r'^login[/]$', redirect_to, {'url': wh_url('/')}),
+    (r'^admin[/]$', redirect_to, {'url': wh_url('/')}),
 
+    (r'^reset-password[/]$', 'django.contrib.auth.views.password_reset', {'password_reset_form': PasswordResetForm}),
+)
 
 urlpatterns += patterns('ivecallocation.allocation.views',
     url(r'^priority_areas/(?P<allocationround_id>\d+)/$', 'priority_areas', name='priority-areas'),
@@ -40,8 +46,8 @@ urlpatterns += patterns('ivecallocation.allocation.views',
 )
 
 urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$',
-        'django.views.static.serve',
-        {'document_root': os.path.join(os.path.dirname(__file__),"static"), 'show_indexes': True}),
+    (r'^static/(?P<path>.*)$',
+    'django.views.static.serve',
+    {'document_root': os.path.join(os.path.dirname(__file__),"static"), 'show_indexes': True}),
 )
 
